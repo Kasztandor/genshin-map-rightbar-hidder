d1fb5845cee03d5403407b34008bc94c8bbb3c60
Twoje Imię Nazwisko <toja@example.com>
2024-03-17 15:50:51 +0100

    Made button "sticking" to rightbar and arrow is rotating

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+let hidden = false
+
 function createHideButton() {
-	document.querySelector('.MapLayout_Rightbar').innerHTML += '<div class="kasztandorsHideButton" style="transition: .25s; position: absolute; top: 0; right: 320px; width: 56px; height: 56px; background-color: #dfdfdf; color: #686868; display: flex; justify-content: center; align-items: center;"><svg style="transform: rotate(180deg);" class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ChevronLeftIcon"><path d="M15.41 7.41 14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg></div>';
+	document.querySelector('.MapLayout_Rightbar').innerHTML += '<div class="kasztandorsHideButton" style="transition: .25s; position: absolute; top: 0; right: 320px; width: 56px; height: 56px; background-color: #dfdfdf; color: #686868; display: flex; justify-content: center; align-items: center;"><svg style="transform: rotate(180deg); transition: .25s;" class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv kasztandorsHideButton" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ChevronLeftIcon"><path d="M15.41 7.41 14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg></div>';
 	document.querySelector('.kasztandorsHideButton').addEventListener('mouseover', () => {
 		document.querySelector('.kasztandorsHideButton').style.backgroundColor = '#cfcfcf';
 	})
@@ -8,13 +10,15 @@ function createHideButton() {
 	})
 	document.querySelector('.kasztandorsHideButton').addEventListener('click', () => {
 		//if have hidden property
-		if (document.querySelector(".MapLayout_Rightbar").classList.contains("hide")) {
-			document.querySelector(".MapLayout_Rightbar").classList.remove("hide");
-			document.querySelector('.kasztandorsHideButton').style.right = '320px';
+		if (hidden) {
+			hidden = false;
+			document.querySelector(".MapLayout_Rightbar").style.right = '0px';
+			document.querySelector('.kasztandorsHideButton').querySelector('svg').style.transform = 'rotate(180deg)';
 		}
 		else {
-			document.querySelector(".MapLayout_Rightbar").classList.add("hide");
-			document.querySelector('.kasztandorsHideButton').style.right = '370px';
+			hidden = true;
+			document.querySelector(".MapLayout_Rightbar").style.right = '-320px';
+			document.querySelector('.kasztandorsHideButton').querySelector('svg').style.transform = 'rotate(0deg)';
 		}
 	})
 }
